ba9d26d2a312338f024323e0a4e0af8d7d8bc1f1
Tyler Fibonacci <gepheum@gmail.com>
2025-04-06 09:47:42 -0400

    move CapitalizeString to its own lib

--- example.cc
+++ example.cc
@@ -4,86 +4,13 @@
 //   bazel run :example
 
 #include <iostream>
-#include <type_traits>
 
 #include "absl/container/flat_hash_set.h"
 #include "absl/status/statusor.h"
-#include "absl/strings/ascii.h"
 #include "absl/time/time.h"
-#include "absl/types/optional.h"
 #include "soia.h"
 #include "soiagen/user.h"
-
-// CapitalizeStrings recursively capitalizes all the strings found within a
-// soia value.
-
-template <typename T>
-typename std::enable_if_t<!soia::reflection::IsRecord<T>()>
-CapitalizeStrings(T&);
-void CapitalizeStrings(std::string& s);
-template <typename T>
-void CapitalizeStrings(std::vector<T>& vector);
-template <typename T, typename GetKey>
-void CapitalizeStrings(soia::keyed_items<T, GetKey>& vector);
-template <typename T>
-void CapitalizeStrings(absl::optional<T>& optional);
-template <typename T>
-typename std::enable_if_t<soia::reflection::IsRecord<T>()>
-CapitalizeStrings(T& record);
-
-template <typename T>
-typename std::enable_if_t<!soia::reflection::IsRecord<T>()>
-CapitalizeStrings(T&) {}
-
-void CapitalizeStrings(std::string& s) { absl::AsciiStrToUpper(&s); }
-
-template <typename T>
-void CapitalizeStrings(std::vector<T>& vector) {
-  for (T& value : vector) {
-    CapitalizeStrings(value);
-  }
-}
-
-template <typename T, typename GetKey>
-void CapitalizeStrings(soia::keyed_items<T, GetKey>& vector) {
-  for (T& value : vector) {
-    CapitalizeStrings(value);
-  }
-}
-
-template <typename T>
-void CapitalizeStrings(absl::optional<T>& optional) {
-  if (optional.has_value()) {
-    CapitalizeStrings(*optional);
-  }
-}
-
-template <typename T>
-struct CapitalizeStringsVisitor {
-  T& record;
-
-  template <typename Getter, typename Value>
-  void operator()(soia::reflection::struct_field<Getter, Value>) {
-    CapitalizeStrings(Getter()(record));
-  }
-
-  template <typename Const>
-  void operator()(soia::reflection::enum_const_field<Const>) {}
-
-  template <typename Option, typename Value>
-  void operator()(soia::reflection::enum_value_field<Option, Value>) {
-    auto* value = Option::get_or_null(record);
-    if (value != nullptr) {
-      CapitalizeStrings(*value);
-    }
-  }
-};
-
-template <typename T>
-typename std::enable_if_t<soia::reflection::IsRecord<T>()>
-CapitalizeStrings(T& record) {
-  soia::reflection::ForEachField<T>(CapitalizeStringsVisitor<T>{record});
-}
+#include "string_capitalizer.h"
 
 int main() {
   // REFERRING TO GENERATED SYMBOLS
